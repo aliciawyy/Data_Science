2fbed086e34027a35a5befac3b488e5aa7e326a7
Alicia W <rainingilove@gmail.com>
2014-06-26 18:44:31 +0200

    Update Fibonacci with cache.

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,22 +4,26 @@
 
 public class Fibonacci
 {
+    public static void Init(int N)
+    {
+        value = new long[N+1];
+        for ( int i = 0; i < N+1; ++i ) { value[i] = -1; }
+        value[0] = 0;
+        value[1] = 1;
+    }
     public static long F(int N)
     {
-        if ( N == 0 ) return 0;
-        if ( N == 1 ) return 1;
-        
-        /**
-         * \bug Recursive calls shouldn't address subproblems that overlap.
-         */
-        return F(N-1) + F(N-2);
+        if ( value[N] < 0 ) { value[N] = F(N-1)+F(N-2); }
+        return value[N];
     }
     
     public static void main(String[] args)
     {
         int N = StdIn.readInt();
-        
+        Init(N);
         for ( int i = 0; i < N; i++ )
             StdOut.println(i + " " + F(i));
     }
+    
+    private static long[] value;
 }
